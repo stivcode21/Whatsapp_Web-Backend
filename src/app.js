81673a6c2763cff86bf0f9182d92f8761c8d8704
TEO <alfredo.ssm@gmail.com>
2024-06-20 00:04:11 -0400

    Cambios menores

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
-const express = require('express');
+const express = require('express')
+const config = require('./config')
+const PORT = config.port || 3000;
 
-const app = express();
+const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>WhatsApp Backend Clone!</h1>');
-});
+  res.send('<h1>WhatsApp Backend Clone!</h1>')
+})
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
-});
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
+})
+
+module.exports = app
