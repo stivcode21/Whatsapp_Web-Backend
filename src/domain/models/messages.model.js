1af8c8e45080a9b3e169421314ea153f24929fd3
Jesus24-Dev <siritjesus24@gmail.com>
2024-08-10 13:57:05 -0400

    Modelo de mensajes creado

--- src/domain/models/messages.model.js
+++ src/domain/models/messages.model.js
@@ -1,17 +1,44 @@
-const { DataTypes, Model } = require("sequelize")
-const { connect } = require("../../infrastructure/database/connection")
-const { Chats } = require("./chats.models")
+import { Model, DataTypes } from "sequelize";
+import { sequelize } from "../../infrastructure/database/connection.js";
 
 //? Se tiene que agregar foreign de usuarios.
 class Messages extends Model {}
 
-Messages.init({
+Messages.init(
+  {
+    message_id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    chat_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.TEXT,
+    },
+    multimedia: {
+      type: DataTypes.BLOB,
+    },
+    dataSent: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+    },
+    isRead: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
+  },
+  {
+    sequelize: connect(),
+    tableName: "Messages",
+    timestamps: false,
+  }
+);
 
-}, {
-  sequelize: connect(),
-  tableName: "Mensajes"
-})
-
-Messages.belongsTo(Chats)
-
-module.exports = { Messages}
+export default Messages;
