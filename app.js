6ecb8e4cc344b7d517904215786caedc751da2e2
Jesus24-Dev <siritjesus24@gmail.com>
2024-08-10 14:04:08 -0400

    Conexion a la base de datos con los modelos Chats y Mensajes

--- app.js
+++ app.js
@@ -1,46 +1,54 @@
-import express from "express"
-import cors from "cors"
-import { connectDB } from "./src/infrastructure/database/connection.js"
-import userRouter from "./src/routes/user.routes.js"
-import { PORT } from "./config.js"
-import cookieParser from "cookie-parser"
+import express from "express";
+import cors from "cors";
+import { connectDB } from "./src/infrastructure/database/connection.js";
+import userRouter from "./src/routes/user.routes.js";
+import { PORT } from "./config.js";
+import cookieParser from "cookie-parser";
 
-import { User, Contacts } from "./src/domain/models/index.js"
-import contactsRouter from "./src/routes/contacts.routes.js"
+import { User, Contacts, Chats, Messages } from "./src/domain/models/index.js";
+import contactsRouter from "./src/routes/contacts.routes.js";
 
-const app = express()
+const app = express();
 
 // Middlewares
 
-app.use(cors({
-  origin: "http://localhost:3000",
-  credentials: true
-}))
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
 
-app.use(express.json({
-  limit: "1mb"
-}))
+app.use(
+  express.json({
+    limit: "1mb",
+  })
+);
 
-app.use(cookieParser())
+app.use(cookieParser());
 
 // Routes
 
-app.use("/user", userRouter)
-app.use("/contacts", contactsRouter)
+app.use("/user", userRouter);
+app.use("/contacts", contactsRouter);
 
 // DataBase
 
-connectDB()
+connectDB();
 
 try {
-  await User.sync()
-  console.log("Table Users created")
-  await Contacts.sync({ force: true })
-  console.log("Table Contacts created")
+  await User.sync();
+  console.log("Table Users created");
+  await Contacts.sync({ force: true });
+  console.log("Table Contacts created");
+  await Chats.sync();
+  console.log("Table Chats created");
+  await Messages.sync();
+  console.log("Table Messages created");
 } catch (error) {
-  console.log(error)
+  console.log(error);
 }
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`)
-})
+  console.log(`Server is running on port ${PORT}`);
+});
