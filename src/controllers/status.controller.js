75566f88698265d529e1c72d42e994e3c332161d
Jesus24-Dev <siritjesus24@gmail.com>
2024-08-16 17:22:26 -0400

    Controlador y ruta para eliminar estados manualmente

--- src/controllers/status.controller.js
+++ src/controllers/status.controller.js
@@ -6,12 +6,31 @@ class StatusController {
     const { description, multimedia } = req.body;
 
     try {
-      const result = await Status.create({
+      await Status.create({
         user_id,
         description,
         multimedia,
       });
-      res.status(201).json(result);
+      res
+        .status(201)
+        .json({ status: "success", message: "A new status created!" });
+    } catch (error) {
+      res.status(500).json({ name: error.name, message: error.message });
+    }
+  }
+
+  static async deleteStatusManually(req, res) {
+    const { user_id, status_id } = req.params;
+    try {
+      await Status.destroy({
+        where: {
+          user_id,
+          status_id,
+        },
+      });
+      res
+        .status(204)
+        .json({ status: "success", message: "Status deleted succesfully" });
     } catch (error) {
       res.status(500).json({ name: error.name, message: error.message });
     }
--- src/routes/status.routes.js
+++ src/routes/status.routes.js
@@ -3,5 +3,9 @@ import { Router } from "express";
 const statusRouter = Router();
 
 statusRouter.post("/:user_id/add", StatusController.addNewStatus);
+statusRouter.delete(
+  "/:user_id/delete/:status_id",
+  StatusController.deleteStatusManually
+);
 
 export default statusRouter;
