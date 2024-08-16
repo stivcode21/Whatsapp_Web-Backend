2b8f517e836016689cb118788dd620a69d0ba80b
Jesus24-Dev <siritjesus24@gmail.com>
2024-08-16 17:39:57 -0400

    Borrar estados automaticamente despues de 24h

--- src/controllers/status.controller.js
+++ src/controllers/status.controller.js
@@ -1,16 +1,33 @@
 import { Status } from "../domain/models/index.js";
 
+class newStatus {
+  constructor(id) {
+    this.id = id;
+    /*PARA PROBAR  */
+    setTimeout(() => this.deleteStatus24H(), 10000);
+  }
+  async deleteStatus24H() {
+    await Status.destroy({
+      where: {
+        status_id: this.id,
+      },
+    });
+  }
+}
+
 class StatusController {
   static async addNewStatus(req, res) {
     const { user_id } = req.params;
     const { description, multimedia } = req.body;
 
     try {
-      await Status.create({
+      const result = await Status.create({
         user_id,
         description,
         multimedia,
       });
+      const idToDelete = result.toJSON();
+      const status = new newStatus(idToDelete.status_id);
       res
         .status(201)
         .json({ status: "success", message: "A new status created!" });
